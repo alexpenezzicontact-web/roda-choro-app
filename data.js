5afa091ab9a4a08d2db9ddc29a3782df5397c261
alexpenezzicontact-web <alexpenezzicontact@gmail.com>
2026-09-16 09:41:06 +0200

    Add initial researched choro library

---
+++ data.js
@@ -0,0 +1,282 @@
+window.RODA_LIBRARY = [
+{
+ id:"carinhoso", official:true, title:"Carinhoso", composer:"Pixinguinha / João de Barro",
+ baseKey:"F", meter:"2/4", form:"A (2x) · B (2x) · coda", status:"Conferida",
+ source:"Acervo Pixinguinha/IMS e catálogo Casa do Choro; mapa funcional cotejado com a partitura cifrada em F.",
+ notes:"Tom-base F. Há diferenças de detalhe entre edições e harmonizações de roda; esta versão preserva a condução cromática e os baixos mais usados na edição consultada.",
+ sources:[["Acervo Pixinguinha/IMS","https://pixinguinha.com.br/discografia/carinhoso-2/"],["Casa do Choro","https://acervo.casadochoro.com.br/works/view/472"]],
+ chart:`[A]
+F | F+ | F6 | F+ | F | F+ | F6 | F7
+Am | Am(#5) | Am6 | Am(#5) | Am | Am(#5) | Am6 | A7
+Dm | G7 | C7 | F7 | Bb | D7 | Gm | G7
+Bbm6/Db | C7 | F | Db7 | C7 | F :: 1ª casa | C7
+F :: 2ª casa | E7
+
+[B]
+Am | Am/G | Dm/F | E7 | Am | Am | Ab7 | C/G
+A7 | D7 | G7 | C | Db7 | Gm/D | C7/E | F
+F | E7 | E7 | Gm7 | C7 | F | A7 | Dm
+A7/E | Dm/F | F7/Eb | Bb/D | D7/C | Gm/Bb | Bbm6 | F
+(F#°) | C7 | F | Db7 | C7 | F :: 1ª casa | C7
+F :: 2ª casa | E7
+
+[CODA]
+F | F7/Eb | Bb/D | Bbm6/Db | F`
+},
+{
+ id:"tico-tico-no-fuba", official:true, title:"Tico-Tico no Fubá", composer:"Zequinha de Abreu",
+ baseKey:"Am", meter:"2/4", form:"Intro · A A · B B · A · C C · A", status:"Conferida",
+ source:"Casa do Choro registra partitura cifrada e edição melodia+cifra; mapa em Lá menor alinhado à tonalidade tradicional da edição de roda consultada.",
+ notes:"As três partes passam por Am, A e C. O app transpõe a obra inteira pelo mesmo intervalo, preservando essas relações.",
+ sources:[["Casa do Choro","https://acervo.casadochoro.com.br/works/view/78"]],
+ chart:`[INTRO]
+Am | E7 | Am | E7
+
+[A]
+Am | E7 | E7 | Am A7 | Dm | Am | B7 | E7
+Am | E7 | E7 | Am A7 | Dm | Am | E7
+
+[B · A MAIOR]
+A | A | A | A | E7 | E7 | E7 | E7
+A | A | A | F#7 | Bm | D A° | A F#7 | Bm E7
+A | E7(#5)
+
+[C · DÓ MAIOR]
+Am | C A7 | Dm | G7 | C | C | Dm | G7
+C | C A7 | Dm | G7 | C | F C° | C A7 | Dm G7
+C | C A7 | Dm | G7 | C | C | Dm | G7
+C | C A7 | Dm | G7 | C | F C° | C A7 | Dm G7
+C | E7 :: volta ao A`
+},
+{
+ id:"brasileirinho", official:true, title:"Brasileirinho", composer:"Waldir Azevedo",
+ baseKey:"A", meter:"2/4", form:"parte maior · parte menor · D.S. al Coda", status:"Conferida",
+ source:"Edições Todamérica catalogadas pela Casa do Choro, cotejadas com o mapa harmônico do songbook de trabalho.",
+ notes:"Tom-base A, com seção menor em Am. Essa mudança modal faz parte da estrutura; ao transpor, maior e menor se deslocam juntos.",
+ sources:[["Casa do Choro","https://acervo.casadochoro.com.br/works/view/4852?letra=T"]],
+ chart:`[INTRO / CHAMADA]
+A6 | % | Am6 | % | A6 | E7
+A6 :: 1ª casa | E7 | A6 :: 2ª casa
+
+[PARTE MAIOR]
+A6 | % | E7 | % | A6 | %
+E7 | % | A6 | % | E7 | %
+A6 | % | E7 | % :: To Coda
+
+[PARTE MENOR]
+Am | % | E7(b9) | % | % | %
+D° | Am/C | Am6 :: 1ª casa | % | D#° | %
+E7(b9) | % | D° | Am/C | Dm :: 2ª casa | %
+Am | % | B7 | E7(b9) | Am :: D.S. al Coda | %
+
+[CODA]
+A6 | % | E7 | A6 | % | %
+E7 | A | A6`
+},
+{
+ id:"noites-cariocas", official:true, title:"Noites Cariocas", composer:"Jacob do Bandolim",
+ baseKey:"G", meter:"2/4", form:"A · B · D.S. al Coda", status:"Conferida",
+ source:"Catálogo Casa do Choro (melodia e cifra) e mapa harmônico em G cotejado com transcrição de repertório.",
+ notes:"Tom de roda G. Mantidas as dominantes secundárias, diminutos e modulações características.",
+ sources:[["Casa do Choro","https://acervo.casadochoro.com.br/works/view/5334"]],
+ chart:`[A]
+G | G | Bb° | D7/A | G | E7 | Am | E7/B
+Am/C | Am | Am(maj7) | Am7 | Am6 | Am/E | D7 | Am D7
+G | E7 | Am | D7 | G | G | Bb° | D7/A
+G | E7 | Am | E7/B | Am/C | C | Bb° | G/B
+E7 | A7 | G :: 1ª casa | D7
+G :: 2ª casa
+
+[B]
+G | G | Dm | G7 | C | C | A7 | A7
+Dm | A7/E | Dm/F | F | F#° | C/G | E7/G# | Am
+B7 | E | C#m | F#m | B7 | E | G7 | Dm
+G7 | C | C | A7 | A7 | Dm | A7/E | Dm/F
+F | B7/D# | E7/D | A7/C# | Dm | G7
+C :: 1ª casa | C :: 2ª casa | C#7 | D7 :: D.S. al Coda
+
+[CODA]
+G | G | G | F#7 | F7 | E7 | A7 | D7
+G | G | C#° | C#° | G | F#7 | F7 | E7
+A7 | D7 | G | G`
+},
+{
+ id:"doce-de-coco", official:true, title:"Doce de Coco", composer:"Jacob do Bandolim",
+ baseKey:"G", meter:"2/4", form:"A · B B · A · Fine", status:"Conferida",
+ source:"Casa do Choro e transcrição harmônica de repertório em G; conduções de baixo preservadas quando relevantes.",
+ notes:"Há versões cantadas com reharmonizações diferentes. Esta é uma leitura instrumental de roda, em G.",
+ sources:[["Casa do Choro","https://acervo.casadochoro.com.br/works/view/5354"]],
+ chart:`[A]
+G | Bm/F# | Am/E | D7/F# | G | Bm/F# | Am/E | D7/F#
+G | Bm/F# | Dm/F | E7 | Am | Am(maj7) | Am7 | Am(maj7)
+Am | Am(maj7) | Am7 | D7 | G | F#7 | F | E7
+Am | E7 | Am | Cm6 | D7 | Gm | Gm(#5) | Gm6
+Gm(#5) | Gm | Gm(#5) | Gm6 | Gm(#5) | Gm | G7 | Cm
+G | Cm | C#° | G | E7 | Am | D7 | G
+
+[B]
+G | Dm/F | E7 | Am | D7 | G | G/B | Bb°
+Am/C | D7/F# | G/B | Bb° | Am | D7/F# | Bm | Bb°
+Am/C | D7/F# | G/B | F#7 | B | G#m | C#m | F#7
+B7 | E7 | Am | A7/C# | D7 | D7/C | G/B | Bb°
+Am | D7/F# | Dm/F | E7 | Am | B7 | Em | Cm6
+G/B | E7 | A7 | D7 | G :: Fine`
+},
+{
+ id:"um-a-zero", official:true, title:"Um a Zero (1 x 0)", composer:"Pixinguinha / Benedito Lacerda",
+ baseKey:"C", meter:"2/4", form:"A A' · B · A · C C · A' · coda", status:"Conferida",
+ source:"Casa do Choro registra edição de melodia e cifra funcional; mapa em C cotejado com material de repertório Choro Vivo/Brazil Real Book.",
+ notes:"Tom-base C. Algumas edições diferem na grafia de diminutos enharmônicos; aqui usamos a grafia funcional da versão consultada.",
+ sources:[["Casa do Choro","https://acervo.casadochoro.com.br/works/view/7225"]],
+ chart:`[A]
+G7 | C | G7 | C7 | C7/Bb | F/A | Fm/Ab | C/G
+D7 | G7 | G7 | C | G7/D | A7/C# | Dm | Fm6
+C | G7 :: 1ª casa
+C :: 2ª casa
+
+[B]
+C | G | Bb° | G/B | G | D7 | Am7 | D7
+Am7 | D7 | G | G | Bb° | G/B | G7 | C
+C#° | G/D | E7 | Am7 | D7 | G
+
+[C]
+G | Am7 | D7 | G | G7 | C | G7 | C7
+C7/Bb | F/A | Fm/Ab | C/G | D7 | G7 | C | G7/D
+A7/C# | Dm | Fm6 | C/E | D7 | G7 | C
+F | Gm7 | C7 | C7/E | F | F | Cm6/Eb | D7
+Cm6 | D7/A | D7/F# | Gm7 | Bb° | D7 :: D.S. al Coda
+
+[CODA]
+F/C | D7 | Gm7 | C7 | F | F`
+},
+{
+ id:"lamentos", official:true, title:"Lamentos", composer:"Pixinguinha",
+ baseKey:"D", meter:"2/4", form:"A A · B B · A + coda", status:"Conferida",
+ source:"Acervo Pixinguinha/IMS e Casa do Choro; mapa funcional em D cotejado com a versão instrumental de roda.",
+ notes:"Não confundir com outras obras homônimas. Esta é a composição de Pixinguinha.",
+ sources:[["Casa do Choro","https://acervo.casadochoro.com.br/works/view/3601"],["Acervo Pixinguinha/IMS","https://pixinguinha.com.br/"]],
+ chart:`[INTRO]
+A7 | B° | A7 | Em | A7
+
+[A]
+D | % | D° | % | D | % | (Fm/C#)
+Am6 | B7 | Em | F#7 | Bm | C#7
+F# | D#m | G#m | C#7 | F#7 | B7 | E7 | A7
+D | D7 | G | C7 | (Gm6) | D | B7 | E7
+A7 | D :: 1ª casa | % | D :: 2ª casa | F#7
+
+[B]
+Bm | Bm(maj7) | Bm7 | Bm6 | B7 | Em | Em(maj7)
+Em7 | Em6 | F#7 | % | Bm | %
+G7 | G#° | F#7 | % | Bm | Bm(maj7)
+Bm7 | Bm6 | B7 | Em | Em(maj7) | Em7 | Em6
+F#7 | % | Bm | % | G7 | G#° | F#7
+Bm :: 1ª casa | F#7 | Bm :: 2ª casa | C° | (Bb7) | A7
+
+[CODA]
+D | D7 | G | F#7 | B7 | E7 | A7
+Am7 D7 :: 1ª casa | D :: 2ª casa`
+},
+{
+ id:"pedacinhos-do-ceu", official:true, title:"Pedacinhos do Céu", composer:"Waldir Azevedo",
+ baseKey:"G", meter:"2/4", form:"A · B · D.S. al Coda", status:"Conferida",
+ source:"Casa do Choro (edições Todamérica) e transcrição harmônica em G cotejada com gravação/repertório.",
+ notes:"Tom-base G, muito usado nas transcrições de roda consultadas.",
+ sources:[["Casa do Choro","https://acervo.casadochoro.com.br/works/view/4868"]],
+ chart:`[A]
+G | B7/F# | Em | Em/D | Bm7(b5) | E7
+Am | E7/B | Am/C | Cm6 | D7
+G/B | Bb° | Am | E7/B | Am/C | E7/B
+A7 | Am7 | Am7(b5) | D7
+G | B7/F# | Em | Em/D | Bm7(b5) | E7
+Am | E7/B | Am/C | Cm6 | D7
+Dm7 | G7(9) | C6(9) | Cm6 | G6 | Em | Eb7 | D7
+G6 | B7/F# :: To Coda
+
+[B]
+Em | % | C | B7 | Em | % | C | B7
+Bm7(b5) | E7 | Am | E7/B | Am/C | F#7(b9) | B7(b9)
+Em | % | C | B7 | Em | % | C | B7
+Bm7(b5) | E7 | Am | E7 | Am | F#m7(b5) | Em
+C7 | B7 | Em | Eb7 | D7 :: D.S. al Coda
+
+[CODA]
+G`
+},
+{
+ id:"brejeiro", official:true, title:"Brejeiro", composer:"Ernesto Nazareth",
+ baseKey:"A", meter:"2/4", form:"A A · B B · A · Fine", status:"Conferida · mapa funcional",
+ source:"Edição oficial ‘melodia e cifra’ do projeto Ernesto Nazareth 150 Anos/IMS, cotejada com a partitura original em domínio público.",
+ notes:"Tom-base A. O mapa privilegia a função harmônica para leitura de roda; baixos de passagem muito específicos do piano foram reduzidos.",
+ sources:[["Ernesto Nazareth 150 Anos / IMS","https://ernestonazareth150anos.com.br/works/view/31"],["Casa do Choro","https://acervo.casadochoro.com.br/works/view/3543"]],
+ chart:`[INTRO]
+A | E7 | A | E7 | A
+
+[A]
+E7 | A | E7 | A | E7 | A
+C#7 | F#m | B7 | E7 | A | Bm
+E7 | A | E7 | A | E7 | A
+E7 | C#7 | F#m | B7 | E7 | A
+Bm | E7 | A :: Fine
+
+[B]
+B7 | E | B7 | E | B7 | E
+G#m | D#7 | G#m | C#7 | F#m | B7
+E | B7 | E | E7 | A | E
+B7 | E | B7 | E :: D.S. ao Fine`
+},
+{
+ id:"odeon", official:true, title:"Odeon", composer:"Ernesto Nazareth",
+ baseKey:"Dm", meter:"2/4", form:"A B A C A · adaptação de roda", status:"Conferida · edição IMS",
+ source:"Adaptação oficial ‘melodia e cifra’ do projeto Ernesto Nazareth 150 Anos/IMS. A edição para roda transpõe a peça para Dm; o original para piano é em C#m.",
+ notes:"O site oficial explica que as adaptações melodia+cifra podem alterar tonalidades para facilitar a prática de conjunto. Por isso mostramos Dm como tom-base desta edição de roda.",
+ sources:[["Ernesto Nazareth 150 Anos / IMS","https://ernestonazareth150anos.com.br/works"],["Casa do Choro","https://acervo.casadochoro.com.br/works/view/3657"]],
+ chart:`[A]
+Dm | Dm/F A7/E | Dm | D/C
+Gm/Bb | D7/A | Gm | Gm/F
+Em7(b5) | Bb7/D A7/C# | Dm/F | A7/E
+Dm | D/C | Gm/Bb D7/A | Gm Bbm6/F
+
+[B]
+G7/D | C7/E | F | F/A
+C7/G | F#° | Gm/Bb | F/C
+B°/C | C7 | F | F/C
+G7/D | C7/E | F | F/A C7/G
+
+[A · VOLTA]
+Dm | Dm/F A7/E | Dm | D/C
+Gm/Bb | D7/A | Gm | Bbm6/F
+A7/E | A7 | Dm | Dm
+
+[C]
+F#° | C7/G | C7 | F
+F#° | C7/G | C7 | F
+F#° | Gm | A7/E | Dm
+B° | F/C | G7/C | C7
+
+[A · FINAL]
+Dm | Dm/F A7/E | Dm | D/C
+Gm/Bb | D7/A | Gm | Bbm6/F
+A7/E | A7 | Dm :: Fine`
+},
+{
+ id:"menina-flor", official:false, personalBase:true, title:"Menina Flor", composer:"Luiz Bonfá / Maria Helena Toledo",
+ baseKey:"F", meter:"2/4", form:"transcrição da partitura do usuário", status:"Fonte primária do usuário",
+ source:"Partitura fornecida pelo usuário para o songbook de 12/09/2026.",
+ notes:"Música extra da sua biblioteca pessoal. Cada caixa corresponde a um compasso na transcrição.",
+ sources:[],
+ chart:`[1–16]
+F | D7 | G7 | G7 | Gm7 | C7 | F°7 | F°7
+F | Bm7(b5) | A | F#m | Bm7 | E7 | Am D7 | Gm C7
+
+[17–32]
+F | D7 | G7 | G7 | Gm7 | C7 | Cm7 | F7
+Bb | Eb7 | Am7 | D7 | Gm7 | C7 | F°7 | F7
+
+[33–40]
+Bb | Eb7 | Am7 | D7 | Gm7 | C7 | F | C7 :: D.S. al Coda
+
+[CODA]
+F | D7 | G7 | G7 | Gb7 | Gb7 | F | F | F | F | F`
+}
+];
